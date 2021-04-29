5d0c8dc3e8be26278b2530f39169509a316e4cdd
Roberto Salazar <roberto.salazar@upaep.mx>
2021-04-29 14:45:45 -0500

    Sesión 40 - 29/04/2021

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { from } from 'rxjs';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FootherComponent } from './components/foother/foother.component';
+import { BodyComponent } from './components/body/body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FootherComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule
